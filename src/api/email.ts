b21fa2be48f2cfefd1d0d7f93dd99632ad60c189
Sirius Deis <falebsy@gmail.com>
2023-07-28 23:50:31 +0200

    add ejs and converting it to text to email transporter

--- src/api/email.ts
+++ src/api/email.ts
@@ -1,6 +1,12 @@
 import nodemailer from 'nodemailer';
+import ejs from 'ejs';
+import htmlToText from 'html-to-text';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const { EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS } = process.env;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const transporter = nodemailer.createTransport({
   host: EMAIL_HOST,
@@ -11,8 +17,32 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const sendEmail = async (to: string, subject: string, text: string, html: string) => {
-  const options = { from: `< Recipe Revere team ${EMAIL_USER}>`, to, subject, text, html };
+try {
+  transporter.verify();
+} catch (error) {
+  throw error;
+}
+
+const sendEmail = async (
+  to: string,
+  subject: string,
+  template: string,
+  context: { title: string; firstName?: string; link: string },
+) => {
+  const rendered = await ejs.renderFile(`${__dirname}/templates/${template}.ejs`, {
+    ...context,
+    homeLink: '',
+    logo: '',
+    from: 'RecipeRevere Team',
+  });
+
+  const options = {
+    from: `< RecipeRevere Team ${EMAIL_USER}>`,
+    to,
+    subject,
+    html: rendered,
+    text: htmlToText.convert(rendered),
+  };
 
   try {
     await transporter.sendMail(options);
