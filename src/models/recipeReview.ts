65e3f80d9ad8dbf5ac7af259d848a883c9bc73c7
Sirius Deis <falebsy@gmail.com>
2023-08-06 00:15:21 +0200

    add a model for recipe review

---
+++ src/models/recipeReview.ts
@@ -0,0 +1,36 @@
+import { Schema, Model, model } from 'mongoose';
+
+interface IRecipeReview {
+  id: string;
+  recipeId: string;
+  userId: string;
+  rating: number;
+  review: string;
+}
+
+type RecipeReviewModel = Model<IRecipeReview, {}>;
+
+const recipeReviewSchema = new Schema<IRecipeReview, RecipeReviewModel>({
+  recipeId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  review: {
+    type: String,
+    maxlength: 256,
+  },
+});
+
+const RecipeReview = model<IRecipeReview>('RecipeReview', recipeReviewSchema);
+
+export default RecipeReview;
