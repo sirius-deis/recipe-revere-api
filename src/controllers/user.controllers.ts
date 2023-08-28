9f58db6eacd2819b05f657cfa0c69b0cf45ccf22
Sirius Deis <falebsy@gmail.com>
2023-08-28 22:26:03 +0200

    add a route for resetting a forgotten password

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -30,6 +30,29 @@ export const activateAccount = catchAsync(
 
 export const resetPassword = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void | NextFunction> => {
-    //TODO:
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const foundToken = await Token.findOne({ token });
+
+    if (!foundToken) {
+      return next(new AppError('There is no such token', 404));
+    }
+
+    const user = await User.findById(foundToken.userId);
+
+    if (!user) {
+      return next(new AppError('There is no such user for provided token. Please try again', 404));
+    }
+
+    if (await user.comparePasswords(password)) {
+      return next(new AppError("A new password can't be the same as previous one", 201));
+    }
+
+    user.password = password;
+
+    await user.save();
+
+    res.status(200).json({ message: 'Your password was changed successfully. Please login again' });
   },
 );
