5b1c1ad019a59dd30a4949f076d7f8f8e9a38bf5
Sirius Deis <falebsy@gmail.com>
2024-03-21 22:51:00 +0100

    replace invoking functions in a conversation resolver with invoking them from a conversationUtils file

--- src/resolvers/conversationResolver.ts
+++ src/resolvers/conversationResolver.ts
@@ -2,27 +2,14 @@ import { GraphQLError } from "graphql";
 import Conversation, { IConversationType } from "src/models/conversation";
 import { IUserType } from "src/models/user";
 import authWrapper from "src/utils/auth";
+import {
+  checkIfConversationExists,
+  checkIfUserIsInConversation,
+} from "src/utils/conversationUtils";
 
 type QueryOptionsType = {
   name?: {};
   tags?: {};
-};
-
-const checkIfConversationExists = async (
-  conversationId: string
-): Promise<IConversationType | never> => {
-  const conversation = await Conversation.findById(conversationId);
-
-  if (!conversation) {
-    throw new GraphQLError("Conversation not found", {
-      extensions: {
-        code: "NOT_FOUND",
-        http: { status: 404 },
-      },
-    });
-  }
-
-  return conversation;
 };
 
 const checkIfUserHasRights = async (
@@ -31,31 +18,6 @@ const checkIfUserHasRights = async (
 ): Promise<void | never> => {
   if (conversation.creatorId.equals(userId)) {
     throw new GraphQLError("You are not the creator of this conversation", {
-      extensions: {
-        code: "NOT_AUTHORIZED",
-        http: { status: 401 },
-      },
-    });
-  }
-};
-
-const checkIfUserIsInConversation = async (
-  conversation: IConversationType,
-  userId: string,
-  truthy: boolean = true
-): Promise<void | never> => {
-  if (
-    truthy &&
-    conversation.members.find((member) => member._id.equals(userId))
-  ) {
-    throw new GraphQLError("You are already in this conversation", {
-      extensions: {
-        code: "NOT_AUTHORIZED",
-        http: { status: 401 },
-      },
-    });
-  } else if (conversation.members.find((member) => member._id.equals(userId))) {
-    throw new GraphQLError("You are not in this conversation", {
       extensions: {
         code: "NOT_AUTHORIZED",
         http: { status: 401 },
